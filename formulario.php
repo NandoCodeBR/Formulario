4a016c09e736443e6b93212d8bf4610120f44ebe
NandoCodeBR <fernando.muniz@outlook.com.br>
2019-09-28 14:31:32 -0300

    formulario

---
+++ formulario.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$nome = $_GET['TXTnome'];
+$endereco = $_GET["TXTendereco"];
+$idade = $_GET["TXTidade"];
+$cpf = $_GET["TXTcpf"];
+
+echo "Seu nome é $nome, você mora na rua $endereco, tem $idade anos e tem o cpf de número: $cpf";
+
+
+
+
+?>
